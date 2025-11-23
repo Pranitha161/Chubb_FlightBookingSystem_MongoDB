1aab344e80f5c527908c57508c25d55f55b71239
Pranitha <pranithareddy160805@gmail.com>
2025-11-23 09:57:34 +0530

    Modified searchRequest entity

--- src/main/java/com/flightapp/webflux/entity/SearchRequest.java
+++ src/main/java/com/flightapp/webflux/entity/SearchRequest.java
@@ -1,12 +1,17 @@
 package com.flightapp.webflux.entity;
 
 import java.time.LocalDate;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document
 public class SearchRequest {
 	private String fromPlace;
 	private String toPlace;
