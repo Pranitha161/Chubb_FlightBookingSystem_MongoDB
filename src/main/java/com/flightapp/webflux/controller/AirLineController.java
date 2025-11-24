bf7d7f998a3f35a25f7721678f18d8ba43573b6f
Pranitha <pranithareddy160805@gmail.com>
2025-11-24 21:53:44 +0530

    removed unused imports

--- src/main/java/com/flightapp/webflux/controller/AirLineController.java
+++ src/main/java/com/flightapp/webflux/controller/AirLineController.java
@@ -1,7 +1,5 @@
 package com.flightapp.webflux.controller;
 
-import javax.swing.text.Document;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
