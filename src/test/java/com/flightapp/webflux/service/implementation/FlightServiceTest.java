2903ee445bec65c1a4035a275bdd45ee4f466251
Pranitha <pranithareddy160805@gmail.com>
2025-11-24 08:31:50 +0530

    Modified flight service test

--- src/test/java/com/flightapp/webflux/service/implementation/FlightServiceTest.java
+++ src/test/java/com/flightapp/webflux/service/implementation/FlightServiceTest.java
@@ -106,6 +106,13 @@ class FlightServiceTest {
 		assertEquals(HttpStatus.BAD_REQUEST,res.getStatusCode());
 	}
 	@Test
+	void testAddFlightInvalidSeats() {
+		Flight flight=new Flight();
+		flight.setAvailableSeats(5);
+		ResponseEntity<Void> res=flightService.addFlight(flight).block();
+		assertEquals(HttpStatus.BAD_REQUEST,res.getStatusCode());
+	}
+	@Test
 	void testGetFlightsSuccess() {
 		Flight flight=new Flight();
 		flight.setAirlineId("123");
